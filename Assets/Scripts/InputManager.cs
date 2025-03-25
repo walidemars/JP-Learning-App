18fd48d0d61d3f4bcdcfb8c1664e97d3882c8088
walidemars <walidemars52@gmail.com>
2025-03-25 07:47:52 +0500

    Initial Commit

---
+++ Assets/Scripts/InputManager.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed = 5f;
+
+    private InputAction moveAction;
+
+    private void Start()
+    {
+        moveAction = InputSystem.actions.FindAction("Move");
+    }
+    void Update()
+    {
+        Vector2 readMovement = moveAction.ReadValue<Vector2>();
+
+        Vector3 movement = new Vector3(readMovement.x, readMovement.y, 0);
+        movement.Normalize();
+        transform.position += movement * moveSpeed * Time.deltaTime;
+        //transform.position.Normalize();
+    }
+}
